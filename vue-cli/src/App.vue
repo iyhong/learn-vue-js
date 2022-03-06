9b400c83955d9f99c2aa03dbe47e21eeed33e7b7
iyhong <loverman85@hanmail.net>
2022-03-06 23:35:34 +0900

    프로젝트 생성 도구 - Vue CLI  - App.vue 설명

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <hello-world></hello-world>
+    <!-- <hello-world></hello-world> -->
   </div>
 </template>
 
