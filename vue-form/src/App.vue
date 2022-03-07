87923648ef61f56e30e59b3a4193755c57cec44e
iyhong <loverman85@hanmail.net>
2022-03-07 17:58:00 +0900

    최종 프로젝트 - 사용자 입력 폼 만들기  - 프로젝트 생성

---
+++ vue-form/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <form @submit.prevent="submitForm">
+      <div>
+        <label for="userName">id: </label>
+        <input id="userName" type="text" v-model="username" />
+      </div>
+      <div>
+        <label for="password">pw: </label>
+        <input id="password" type="password" v-model="password" />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    submitForm() {
+      console.log(this.username, this.password)
+    },
+  },
+}
+</script>
+
+<style></style>
